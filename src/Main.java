b7e3f3a01fdd279ca0bdb2894435e574e5bdf0ab
Orlandopc97 <orlandopc1797@gmail.com>
2024-11-22 14:58:17 -0600

    Constantes en java

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int numero = 10;
+        numero = 15;
+
+        //Asi de declara una constante
+        final Integer valor = 15;
+        //aunque sea un objeto integer se queda como constante
+        //valor = 15;
+
+        System.out.println("Hello world!");
+    }
+}
